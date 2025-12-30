06d958af3f9d586b3a933af0a102a5148b3878d8
Vikramgupte99 <vikramgupte99@gmail.com>
2025-12-30 03:08:21 -0500

    Created function to calculate Total of all added items. same function added to 'Add' and 'Remove' function as callback to update total.

--- script.js
+++ script.js
@@ -5,10 +5,20 @@ $(document).on('click','.add',function () {
     var itemQuantity = parseFloat(row.find('.quantity input').val());
     var subTotal = itemPrice * itemQuantity;
    row.find('.subtotal').html(subTotal.toFixed(2));
+   updateTotal();
 });
 
 //Remove button function
 $(document).on('click', '.remove', function () {
     var row = $(this).closest('tr');
     row.remove();
+    updateTotal();
 });
+
+var updateTotal = function () {
+    total = 0;
+    $('.subtotal').each( function () {
+        total += parseFloat($(this).text());
+    });
+    $('.total span').html(total.toFixed(2));
+};
